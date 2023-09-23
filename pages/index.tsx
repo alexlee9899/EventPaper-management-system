97a7cab01e2f8ef47decba513b53bd03abb6d4e2
yanlinli <lyanlin99@gmail.com>
2023-09-23 23:25:03 +1000

    step5 'feed'change 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,24 @@ import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from '../lib/prisma';
-
+// export const getStaticProps: GetStaticProps = async () => {
+//   const feed = [
+//     {
+//       id: "1",
+//       title: "Prisma is the perfect ORM for Next.js",
+//       content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+//       published: false,
+//       author: {
+//         name: "Nikolas Burk",
+//         email: "burk@prisma.io",
+//       },
+//     },
+//   ]
+//   return { 
+//     props: { feed }, 
+//     revalidate: 10 
+//   }
+// }
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = await prisma.post.findMany({
