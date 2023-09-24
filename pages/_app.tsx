2ce11afe2c1a117c994f5e1b971a67afd6a8091d
yanlinli <lyanlin99@gmail.com>
2023-09-25 09:54:49 +1000

    new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider {...pageProps}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-
   );
 };
 
