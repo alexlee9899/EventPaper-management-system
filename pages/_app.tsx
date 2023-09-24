32507685fbc8cd1ff34b8d3fcd6f3845e172228e
yanlinli <lyanlin99@gmail.com>
2023-09-25 00:06:27 +1000

    new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import { SessionProvider } from 'next-auth/react';
-import { AppProps } from 'next/app';
+// import { AppProps } from 'next/app';
+import { AppProps as NextAppProps } from 'next/app';
+
+interface AppProps extends NextAppProps {
+  pageProps: {
+    session?: any; // 你可以替换 `any` 为你的 session 对象的具体类型
+  };
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
